2b751448bcc585929281caa470be391c6bb26840
KhamidovAkbar <thesh3yx@gmail.com>
2019-01-24 08:22:15 +0900

    ability to replace a chain added

--- blockchain/index.js
+++ blockchain/index.js
@@ -29,6 +29,18 @@ class BlockChain {
 		}
 		return true;
 	}
+
+	replaceChain(newChain) {
+		if (newChain.length <= this.chain.length) {
+			console.log('New chain has fewer blocks than the current one.');
+			return;
+		} else if (!this.isValidChain(newChain)) {
+			console.log('New chain is not valid.');
+		}
+
+		console.log('Replacing the blockchain with the newChain');
+		this.chain = newChain;
+	}
 }
 
 module.exports = BlockChain;
--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,4 +1,4 @@
-const Blockchain = require('./blockchain');
+const Blockchain = require('.');
 const Block = require('./block');
 
 describe('Blockchain', () => {
@@ -19,19 +19,33 @@ describe('Blockchain', () => {
 		expect(bc.chain[bc.chain.length - 1].data).toEqual(givenData);
 	});
 
-	it('checks if the chain is valid', () => {
+	it('validates if the chain is valid', () => {
 		bc2.addBlock('dummyData');
 		expect(bc.isValidChain(bc2.chain)).toBe(true);
 	});
 
-	it('checks if invalidates the chain with corrupted genesis block', () => {
+	it('invalidates the chain with corrupted genesis block', () => {
 		bc2.chain[0].data = 'Changed dummy data';
 		expect(bc.isValidChain(bc2.chain)).toBe(false);
 	});
 
-	it('checks if invlaidates the chain with corrupted block at any point', () => {
+	it('invlaidates the chain with corrupted block at any point', () => {
 		bc2.addBlock('dummy data');
 		bc2.chain[1].data = 'Tempered dummy data';
 		expect(bc.isValidChain(bc2.chain)).toBe(false);
+	});
+
+	it('replaces the blockchain with a valid chain', () => {
+		bc2.addBlock('dummyData');
+		bc.replaceChain(bc2.chain);
+
+		expect(bc.chain).toEqual(bc2.chain);
+	});
+
+	it("doesn't replace the chain with the one has fewer blocks", () => {
+		bc.addBlock('dummyData');
+		bc.replaceChain(bc2.chain);
+
+		expect(bc.chain).not.toEqual(bc2.chain);
 	});
 });
